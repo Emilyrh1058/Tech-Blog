24b4a813cdb2d32ce033629c1d3cefe5c3fe2c12
Emily Salinas <emilyrh1058@gmail.com>
2021-07-22 18:02:43 -0500

    create and edit test successfully

--- public/js/create-post.js
+++ public/js/create-post.js
@@ -1,28 +1,26 @@
-async function createPostHandler(event) {
+async function postHandler(event) {
   event.preventDefault();
-  const title = document.querySelector("#post-title").value.trim();
-  const body = document.querySelector("#post-body").value.trim();
-  
-  if (body) {
-    const response = await fetch("/api/posts", {
-      method: "POST",
+
+  const title = document.querySelector('textarea[name="post-title"]').value.trim();
+  const post_text = document.querySelector('textarea[name="post-text"]').value.trim();
+
+  const response = await fetch('/api/posts', {
+      method: 'post',
       body: JSON.stringify({
-        title,
-        body,
+          title,
+          post_text
       }),
       headers: {
-        "Content-Type": "application/json",
-      },
-    });
+          'Content-Type': 'application/json'           
+      }
+  });
 
-    if (response.ok) {
-      document.location.replace("/dashboard"); 
-    } else {
+  if (response.ok) {
+      document.location.replace('/dashboard');
+      console.log(title, post_text);
+  } else {
       alert(response.statusText);
-    }
   }
 }
 
-document
-  .querySelector("#create-post-btn")
-  .addEventListener("click", createPostHandler);
+document.querySelector('.new-post-form').addEventListener('submit', postHandler);
